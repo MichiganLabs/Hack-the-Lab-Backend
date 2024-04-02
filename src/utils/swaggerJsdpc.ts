09c0109e85b1914dc91651ebb0f3a608309106bc
Jack Pfeiffer <jpfeiffer@michiganlabs.com>
2024-04-02 19:48:18 -0400

    Swagger Js Doc

--- src/utils/swaggerJsdpc.ts
+++ src/utils/swaggerJsdpc.ts
@@ -1,15 +1,27 @@
-const swaggerJsdoc = require("swagger-jsdoc");
+import swaggerJsdoc from "swagger-jsdoc";
 
 const getOpenapiSpecification = () => {
   const options = {
     definition: {
-      openapi: "3.0.0",
+      openapi: "3.1.0",
       info: {
         title: "Hack the Lab 2024 API",
         version: "1.0.0",
       },
+      security: [{
+        apiKey: []
+      }],
+      components: {
+        securitySchemes: {
+          apiKey: {
+            type: 'apiKey',
+            in: 'header',
+            name: 'X-API-KEY'
+          }
+        }
+      }
     },
-    apis: ["./src/controllers/*/*.ts"],
+    apis: ["./src/controllers/**/*.ts", "./src/@types/**/*.d.ts"],
   };
   return swaggerJsdoc(options);
 };
