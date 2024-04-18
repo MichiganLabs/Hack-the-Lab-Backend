ffb14211282c1c4bea5bc55a011970ecff8ea8f0
Jack Pfeiffer <jpfeiffer@michiganlabs.com>
2024-04-17 22:59:49 -0400

    Rat service

---
+++ src/services/rat-service.ts
@@ -0,0 +1,64 @@
+import { Surroundings } from "hackthelab";
+import { acquireLock, getRatPosition, releaseLock, setRatPosition } from "@data";
+import { CellType, Direction } from "@enums";
+import { pgQuery } from "data/db";
+
+export const moveRat = async (userId: string, mazeId: string, direction: Direction): Promise<Surroundings | null> => {
+    let lock = `lock-${userId}-${mazeId}`;
+
+    await acquireLock(lock);
+
+    var position = await getRatPosition(userId, mazeId);
+    var prevPosition = position;
+
+    if (!position) {
+        // This should be the start of the maze.
+        position = { x: 0, y: 0 };
+    }
+
+    console.log(`Rat is currently at (${position.x}, ${position.y})`);
+
+    var didMove = true;
+
+    // TODO: Do maze logic to check whether the rat can move and return cell data.
+    switch (direction) {
+        case Direction.North:
+            position.y -= 1;
+            break;
+        case Direction.East:
+            position.x += 1;
+            break;
+        case Direction.South:
+            position.y += 1;
+            break;
+        case Direction.West:
+            position.x -= 1;
+            break;
+    }
+
+    let surroundings: Surroundings = null;
+
+    if (didMove) {
+        // If the rat moved, update the position
+        await setRatPosition(userId, mazeId, position);
+
+        // This insert could also be distributed through redis events to a
+        // different process, to reduce response times.
+        await pgQuery(
+            "INSERT INTO actions (user_id, maze_id, direction, prev, curr) VALUES ($1, $2, $3, $4, $5)",
+            [userId, mazeId, direction, prevPosition, position]
+        );
+
+        surroundings = {
+            originCell: CellType.cheese,
+            northCell: CellType.open,
+            eastCell: CellType.exit,
+            southCell: CellType.open,
+            westCell: CellType.wall,
+        };
+    }
+
+    releaseLock(lock);
+
+    return surroundings;
+};
