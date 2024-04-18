9f3b76f02a0ed8b3cb3851bd58513cd5507b1f01
Jack Pfeiffer <jpfeiffer@michiganlabs.com>
2024-04-17 22:59:22 -0400

    Move files

--- src/controllers/rat-controller/postMove.ts
+++ src/controllers/rat-controller/postMove.ts
@@ -1,6 +1,25 @@
-import { CellType } from "@enums";
 import { RequestHandler } from "express";
-import { Surroundings } from "hackthelab";
+import { moveRat } from "services/rat-service";
+import { body, matchedData, validationResult } from "utils/custom-validator";
+
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     MoveRequestBody:
+ *       type: object
+ *       properties:
+ *         mazeId:
+ *           type: string
+ *           example: 1234
+ *         direction:
+ *           $ref: '#/components/schemas/Direction'
+ */
+export const moveSchema = [
+  body("mazeId").isString(),
+  body("direction").isDirection(),
+];
 
 /**
  * @swagger
@@ -8,30 +27,54 @@ import { Surroundings } from "hackthelab";
  *   post:
  *     tags: [Rat]
  *     summary: Moves a rat in a direction for a specific maze
+ *     requestBody:
+ *       description: Move request.
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/MoveRequestBody'
  *     responses:
  *       200:
  *         description: Move successful
  *         content:
  *           application/json:
  *             schema:
- *               type: object
- *               properties:
- *                 surroundings:
- *                   $ref: '#/components/schemas/Surroundings'
+ *               $ref: '#/components/schemas/Surroundings'
+ *       400:
+ *         description: Bad request.
+ *       409:
+ *         description: Move unsuccessful.
+ *       500:
+ *         description: Internal server error.
  */
-const postMove: RequestHandler = async (_req, res, next) => {
-  const surroundings: Surroundings = {
-    originCell: CellType.Cheese,
-    northCell: CellType.Open,
-    eastCell: CellType.Exit,
-    southCell: CellType.Open,
-    westCell: CellType.Wall,
-  };
+const move: RequestHandler = async (req, res, next) => {
+  // Check to see if the request is valid.
+  const errors = validationResult(req);
 
-  res.status(200).json(surroundings);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const data = matchedData(req);
+
+  // TODO: Verify `data.mazeId` is a valid maze ID. If not, return with error.
+
+
+  const newCell = moveRat(req.user.id, data.mazeId, data.direction);
+
+  try {
+    if (newCell != null) {
+      res.status(200).json(newCell);
+    } else {
+      res.sendStatus(409);
+    }
+  } catch {
+    res.status(500).json({ error: "Internal server error" });
+  }
 
   next();
   return;
 };
 
-export default postMove;
+export default move;
