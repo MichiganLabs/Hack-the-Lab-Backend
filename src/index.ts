2a2f2277cb4cd181dfd78e45dc3d64d253456341
Jack Pfeiffer <jpfeiffer@michiganlabs.com>
2024-06-06 09:07:22 -0400

    HTL-2 Update limit

--- src/index.ts
+++ src/index.ts
@@ -21,8 +21,8 @@ if (process.env.NODE_ENV === "development") {
 }
 
 const limiter = rateLimit({
-  windowMs: 5 * 60 * 1000, // 5 minutes
-  limit: 100, // Limit each IP to 100 requests per `window` (here, per 5 minutes).
+  windowMs: 1000, // 1 second
+  limit: 50, // Limit each IP to 100 requests per `window` (here, per 5 minutes).
   standardHeaders: "draft-7", // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
 });
