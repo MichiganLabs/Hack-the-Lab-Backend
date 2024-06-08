6e594b741959e949c4c1bf19df14bb267f5c6716
Jack Pfeiffer <jpfeiffer@michiganlabs.com>
2024-06-07 20:18:57 -0400

    HTL-22 Wrong environment variable

--- src/data/db.ts
+++ src/data/db.ts
@@ -5,7 +5,7 @@ const pool = new Pool({
   user: process.env.POSTGRES_USER,
   host: process.env.POSTGRES_HOST,
   database: process.env.POSTGRES_DB,
-  password: process.env.POSTGRES_PASS,
+  password: process.env.POSTGRES_PASSWORD,
   port: parseInt(process.env.POSTGRES_PORT),
 });
 
